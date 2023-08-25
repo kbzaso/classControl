c3c099fd645f7ff4a21d48ab84373eb713662ce3
Alejandro Sáez <alejandro.sapa@gmail.com>
2023-08-25 19:58:41 -0400

    create class

--- src/routes/horarios/+page.server.ts
+++ src/routes/horarios/+page.server.ts
@@ -1,4 +1,4 @@
-import { redirect } from "@sveltejs/kit";
+import { redirect, type Actions } from "@sveltejs/kit";
 import type { PageServerLoad } from "./$types";
 import { client } from "$lib/server/lucia";
 
@@ -20,4 +20,28 @@ export const load: PageServerLoad = async ({ locals }) => {
 		classes,
 		user: session.user,
 	};
+
+};
+
+export const actions: Actions = {
+	// create a new class
+	create: async ({ request, locals }) => {
+		const session = await locals.auth.validate();
+		if (!session) throw redirect(302, "/");
+
+		const formData = await request.formData();
+		let when = formData.get("when");
+		when = new Date(when).toISOString()
+		const level = formData.get("level");
+
+		console.log(formData)
+
+		const newClass = await client.class.create({
+			data: {
+				when: when,
+				level: level,
+			},
+		});
+		throw redirect(302, "/horarios");
+	},
 };
--- src/routes/horarios/+page.svelte
+++ src/routes/horarios/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import Collapse from '$lib/components/Collapse.svelte';
 	import type { PageData } from './$types';
-	
+
 	export let data: PageData;
 	console.log(data);
 </script>
@@ -12,45 +12,49 @@
 	{#each data.classes as training}
 		<Collapse data={training} userId={data.userId} />
 	{/each}
-	<button
-		class="btn backdrop-blur-xl bg-white/10 border-none rounded-full w-16 h-16 fixed bottom-20 right-6"
-		onclick="my_modal_5.showModal()"
-	>
-		<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-			><path fill="#f9e710" d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z" /></svg
+
+	{#if data.user.role === 'ADMIN'}
+		<button
+			class="btn backdrop-blur-xl bg-white/10 border-none rounded-full w-16 h-16 fixed bottom-20 right-6"
+			onclick="my_modal_5.showModal()"
 		>
-	</button>
+			<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+				><path fill="#f9e710" d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z" /></svg
+			>
+		</button>
+	{/if}
 </main>
 
 <!-- ADD USERS -->
 <dialog id="my_modal_5" class="modal modal-bottom sm:modal-middle">
 	<form method="dialog" class="modal-box">
 		<button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
-		<form action="" class="mt-4 flex flex-col gap-4 p-4">
+		<form method="POST" action="?/create" class="mt-4 flex flex-col gap-4 p-4">
 			<h2 class="text-xl uppercase tracking-widest text-yellow-300 text-center">Agregar clase</h2>
-			<label class="text-gray-600" for="start"
+			<label for="when" class="text-gray-600"
 				>Fecha
 
 				<input
 					type="datetime-local"
-					id="start"
-					name="trip-start"
+					id="when"
+					name="when"
 					class="input input-bordered input-primary w-full mt-1 text-left"
 					min="2023-01-01"
 				/>
 			</label>
 
-			<label for="plan" class="text-gray-600 flex flex-col gap-1">
+			<label for="level" class="text-gray-600 flex flex-col gap-1">
 				Nivel
-				<select class="select select-primary w-full" required name="plan">
-					<option>Básico</option>
-					<option>Intermedio</option>
-					<option>Avanzado</option>
+				<select id="level" class="select select-primary w-full" required name="level">
+					<option value="BASIC">Básico</option>
+					<option value="INTERMEDIATE">Intermedio</option>
+					<option value="ADVANCED">Avanzado</option>
 				</select>
 			</label>
 			<button class="btn btn-success" type="submit">Crear</button>
 			<button onclick="my_modal_5.close()" class="btn btn-outline btn-warning" type="reset"
-				>Cerrar</button>
+				>Cerrar</button
+			>
 		</form>
 	</form>
 </dialog>
